3b7bb2162e2f446bc4714659c7aef257de6699f6
Alexis Papadopoulos <github.com@am.wacp.eu>
2022-11-15 17:46:55 +0100

    Initial Sofle push (actual one)

---
+++ keyboards/sofle/keymaps/acp/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#define AUTO_SHIFT_REPEAT
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
